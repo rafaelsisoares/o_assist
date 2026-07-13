7eb8187ee448fcbc509e8a2952667cb96ff326b5
Rafael Soares <r.soares2015@outlook.com>
2026-07-13 20:59:33 -0300

    chore: cria a view de register

--- front/views.py
+++ front/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Login, User
 
 
 def index(request):
     return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        data = request.POST
+
+        if data.is_valid():
+            User.objects.create(**data.cleaned_data)
+            return redirect('login')
+
+    return render(request, 'register.html')
