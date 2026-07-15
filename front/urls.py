6e1af6d059755d2e58f5357735a7e7c5fc8d696b
Rafael Soares <r.soares2015@outlook.com>
2026-07-15 20:28:27 -0300

    feat: cria o endpoint para renderizar o chat

--- front/urls.py
+++ front/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, register
+from .views import index, register, chat
 
 urlpatterns = [
     path('', index, name='login-page'),
     path('register/', register, name='register-page'),
+    path('chat/', chat, name='chat-page'),
 ]
